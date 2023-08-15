76c6e07b1552b6dabb965cdef9d48ee6dab4eb9a
Henry Corredor González <henry.corredor7570@gmail.com>
2023-08-15 17:51:59 -0500

    Creacion db.js: HC

--- src/db.js
+++ src/db.js
@@ -7,10 +7,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const sequelize = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
    {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      logging: false,
+      native: false,
    }
 );
+/*  
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -39,15 +40,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon, Type } = sequelize.models;
+const { } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Pokemon.belongsToMany(Type, { through: 'Pokemon_Type' });
-Type.belongsToMany(Pokemon, { through: 'Pokemon_Type' });
-
+ */
 
 module.exports = {
    ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+   sequelize, // para importart la conexión { conn } = require('./db.js');
 };
