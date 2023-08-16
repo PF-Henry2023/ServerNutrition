3b45f0e8c3347441cf8720e5504074ea03dc2933
Henry Corredor González <henry.corredor7570@gmail.com>
2023-08-16 11:45:42 -0500

    Cambios rama main

---
+++ server/src/models/Users.js
@@ -0,0 +1,61 @@
+const { DataTypes } = require("sequelize")
+
+const Users = (sequelize) => {
+    sequelize.define('Users', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        apellidos: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isUrl: true
+            }
+        },
+        nacimiento: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        contraseña: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: /^(?=.*[A-Z])(?=.*\d).{6,8}$/
+            }
+        },
+        telefono: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        imagen: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isUrl: true
+            }
+        },
+        direccion: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        genero: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isIn: [['Masculino', 'Femenino', 'No binario']]
+            }
+        }
+    });
+}
+
+module.exports = Users; 
