5f87f16479cbebe92869e721eb87f5026c83ca5b
Quena <quenallatafranco@gmail.com>
2023-08-18 15:33:04 -0300

    error fix

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const server = require('./src/app');
-const { sequelize } = require('./src/db');
-const PORT = 3001;
+const server = require("./src/app");
+const { sequelize } = require("./src/db");
+const PORT = process.env.PORT || 3001;
+const DB_HOST = process.env.DB_HOST || "localhost";
 
 sequelize.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log(`Servidor en línea en el puerto: ${PORT}`);
+    console.log(`Server raise in: ${DB_HOST}:${PORT}`);
   });
 });
