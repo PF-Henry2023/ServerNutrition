aa84cee92b764cfd7e6591690494e90fb32c23ef
OfirRoque <113478448+OfirRoque@users.noreply.github.com>
2023-08-18 09:04:21 -0500

    Modelos relaciones (#13)
    * propiedades-español
    
    * estructura-rutas
    
    * modularizacionDeRutas
    
    * Avanze de modelos
    
    ---------
    
    Co-authored-by: Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
    Co-authored-by: PF-Henry2023 <141773841+PF-Henry2023@users.noreply.github.com>
    Co-authored-by: daniel100f <danielvillarraga2@gmail.com>
    Co-authored-by: Daniel Fernando Villarraga Salgado <86505078+daniel100f@users.noreply.github.com>

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,8 @@
 //index todas las rutas
 const { Router } = require('express');
 const router = Router();
-
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-/* const citaHandler=require("../handlers/usersHandler") */
-const usersRouter = require("./usersRouter");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
