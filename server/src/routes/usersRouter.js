07a99996e61a50a050d9bcfa35592b42dd791eb0
Henry Corredor González <henry.corredor7570@gmail.com>
2023-08-18 14:17:05 -0500

    Creacion carpeta utils y archivos validaciones y modificar README.md: HC

--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -2,11 +2,12 @@
 const { Router } = require("express");
 const usersRouter = Router();
 const { createUserHandler, deleteUserHandler, updateUserHanlder, getAllUsersHandler } = require("../handlers/usersHandler");
+const { validateCreateUser } = require ("../Utils/genericFunctions");
 
 // endpoints: ruta de acceso a nuestro backend;
 usersRouter.post("/", createUserHandler);
 usersRouter.delete("/:id", deleteUserHandler);
 usersRouter.put("/:id", updateUserHanlder);
-usersRouter.get("/", getAllUsersHandler);
+usersRouter.get("/", validateCreateUser, getAllUsersHandler);
 
 module.exports = usersRouter;
