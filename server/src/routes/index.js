a74f26dbcbe2b1091ddafd4875ea1ae15613f759
Quena <quenallatafranco@gmail.com>
2023-08-18 15:18:23 -0300

    error fixed, refactorizanding code

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,6 @@
 //index todas las rutas
-const { Router } = require('express');
+const { Router } = require("express");
+const usersRouter = require("./usersRouter.js");
 const router = Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -9,6 +10,5 @@ const router = Router();
 
 router.use("/users", usersRouter);
 /* router.use("/cita",citaHandler); */
-
 
 module.exports = router;
