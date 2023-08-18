a4f3b68736c99138a262d58fa7844575a5801903
Quena <quenallatafranco@gmail.com>
2023-08-18 15:57:07 -0300

    fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const server = require("./src/app");
 const { sequelize } = require("./src/db");
 const { DB_HOST, PORT } = process.env;
+//db_link: "servernutrition-production.up.railway.app"
 
 sequelize.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
