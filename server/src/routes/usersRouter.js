9bc54c5411e1e757d2b9142b2c57be9af7adcfaa
Henry Corredor González <henry.corredor7570@gmail.com>
2023-08-18 12:59:13 -0500

    cambiando datos modelo Nutrionist de español a ingles: HC

--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -1,13 +1,12 @@
 // ruta usuarios
 const { Router } = require("express");
 const usersRouter = Router();
-const { createUserHandler, deleteUserHandler, updateUserHanlder, getUserHandler, getAllUsersHandler } = require("../handlers/usersHandler");
+const { createUserHandler, deleteUserHandler, updateUserHanlder, getAllUsersHandler } = require("../handlers/usersHandler");
 
 // endpoints: ruta de acceso a nuestro backend;
 usersRouter.post("/", createUserHandler);
 usersRouter.delete("/:id", deleteUserHandler);
 usersRouter.put("/:id", updateUserHanlder);
-// usersRouter.get("/", getUserHandler);
 usersRouter.get("/", getAllUsersHandler);
 
 module.exports = usersRouter;
