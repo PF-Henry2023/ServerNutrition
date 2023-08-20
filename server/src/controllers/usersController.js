b85c13600bf90b9efa7011f8bb0a3dcb51246b10
daniel100f <danielvillarraga2@gmail.com>
2023-08-20 15:17:18 -0500

    rutas-jwt

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -1,39 +1,79 @@
-/* logica de usuarios */
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { User } = require("../db");
+require("dotenv").config();
 
 //Crea un usuario en la DB:
-const createUserDB = async (name, lastName, email, birthDate, password, phone, image, address, gender) => {
+const createUserDB = async ({ name, lastName, email, birthDate, password, phone, image, address, gender }) => {
+    
+  
+    const passwordHashed = await bcrypt.hash(password, 8);
+    console.log(passwordHashed);
+  
+    const [user, created] = await User.findOrCreate({
+      where: { email },
+      defaults: {
+        name,
+        lastName,
+        email,
+        birthDate,
+        password: passwordHashed,
+        phone,
+        image, 
+        address, 
+        gender
+      },
+    });
+    if (!created) throw new Error("User already exists");
+  
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+    return token;
+  };
 
-    const newUser = await User.create({name, lastName, email, birthDate, password, phone, image, address, gender});
+  //compropar token para la autenticacion:
 
-    return newUser;
-}
+  const authentication = async ({email,password}) => {
+  
+    const user = await User.findOne({ where: { email } });
+    if (!user) throw new Error("Wrong user or password");
+  
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword || user.email !== email)
+      throw new Error("Wrong user or password");
+  
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+    return token;
+  };
+  
+  //mostrar usuario verificado
+  const getUser = async (token) => {
+    const { id } = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findOne({ where: { id } });
+    return user;
+  };
 
+
+  //Actualizar un usuario:
+  const updateUser = async (token, data) => {
+    const allowedFields = ["name", "lastName", "email","password","phone","image","address"];
+    const updateFields = Object.keys(data);
+    const invalidFields = updateFields.filter(
+      (field) => !allowedFields.includes(field)
+    );
+    if (invalidFields.length > 0) throw new Error("Invalid Fields");
+  
+    const { id } = jwt.verify(token, process.env.SECRET_KEY);
+    await User.update(data, { where: { id } });
+    return {
+      status: "Updated successfully",
+    };
+  };
 //Eliminar un usuario:
-const deleteUser = async (id) => {
+const deleteUser = async (id) => {    
     await User.destroy({where: { id: id}});
 }   
 
-//Actualizar un usuario:
 
-const updateUser = async (id, infoUser) => {
-    const user = await User.findByPk(id)
-    if(!user) throw Error(`Usuario no existe en la base de datos`);
-    const { name, lastName, email, birthDate, password, phone, image, address, gender } = infoUser;//desestructuro la info del usuario que me llega por body
-    if(user){
-        user.name = name
-        user.lastName = lastName
-        user.email = email
-        user.birthDate = birthDate;
-        user.password = password;
-        user.phone = phone;
-        user.image = image;
-        user.address = address;
-        user.gender = gender;
-        await user.save();
-    }
-    return user
-}
 
 //Obtener todos los usuarios:
 const getAllUsers = async () => {
@@ -44,8 +84,28 @@ const getAllUsers = async () => {
 
 
 module.exports = {
-    createUserDB,
-    deleteUser,
+  createUserDB,
+  deleteUser,
     updateUser,
     getAllUsers, 
-}
+    authentication,
+    getUser
+  }
+  /* const updateUser = async (id, infoUser) => {
+      const user = await User.findByPk(id)
+      if(!user) throw Error(`Usuario no existe en la base de datos`);
+      const { name, lastName, email, birthDate, password, phone, image, address, gender } = infoUser;//desestructuro la info del usuario que me llega por body
+      if(user){
+          user.name = name
+          user.lastName = lastName
+          user.email = email
+          user.birthDate = birthDate;
+          user.password = password;
+          user.phone = phone;
+          user.image = image;
+          user.address = address;
+          user.gender = gender;
+          await user.save();
+      }
+      return user
+  } */
--- server/src/handlers/usersHandler.js
+++ server/src/handlers/usersHandler.js
@@ -1,16 +1,54 @@
 /* handler usuarios */
-const { createUserDB, deleteUser, updateUser, getAllUsers } = require("../controllers/usersController");
+const { createUserDB, deleteUser, updateUser, getAllUsers,authentication,getUser } = require("../controllers/usersController");
 
 //crea un usuario en la DB:
-const createUserHandler = async(req, res) => {
+
+// ruta crear usuario y generar token.
+const signup = async (req, res) => {
     const { name, lastName, email, birthDate, password, phone, image, address, gender } = req.body;
     try {
-        const response = await createUserDB( name, lastName, email, birthDate, password, phone, image, address, gender );
-        res.status(201).json(response);
+      const token = await createUserDB({ name, lastName, email, birthDate, password, phone, image, address, gender });
+      res.status(200).header("authorization", `Bearer ${token}`).json({token});
     } catch (error) {
-        res.status(400).json({error: error.message});
+      res.status(400).json({error:error.message});
+    }
+  };
+// ruta crear y verificar el token ingresado
+
+const login = async (req, res) => {
+    const{email,password}=req.body;
+    console.log('login token:', req.body);
+    try {
+      const token = await authentication({email,password});
+      res.status(200).header("authorization", `Bearer ${token}`).json({token});
+    } catch (error) {
+      res.status(404).json({error:error.message});
+    }
+  };
+
+// traer el usuario creado o autorizado
+const user = async(req, res) => {
+    try {
+        const userData = await getUser(req.user);
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(400).json({error:error.message});
     }
 }
+
+const ensureToken = (req, res, next) => {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+      const bearer = bearerHeader.split(" ");
+      const bearerToken = bearer[1];
+      req.user = bearerToken;
+      next();
+    } else {
+      res.status(403).json({ error: "Token not provided" });
+    }
+  };
+
+
 
 //ruta para eliminar un usuario:
 const deleteUserHandler = async(req,res) => {
@@ -24,15 +62,15 @@ const deleteUserHandler = async(req,res) => {
 }
 
 //ruta para actualizar un usuario:
-const updateUserHanlder = async (req,res) => {
-    const { id } = req.params;
-    const { name, lastName, email, birthDate, password, phone, image, address, gender } = req.body;
-    try {
-        await updateUser(id, {name, lastName, email, birthDate, password, phone, image, address, gender });
-        res.status(200).json(`Usuario ${name} actualizado con éxito!`);
-    } catch (error) {
-        res.status(400).json(`Error al actualizar usuario: `, {error:error.message});
-    }
+
+
+const updateUserHanlder = async(req, res) => {
+  try {
+      const status = await updateUser(req.user, req.body);
+      res.status(200).json(status);
+  } catch (error) {
+      res.status(400).json({error:error.message})
+  }
 }
 
 //ruta para obtener todos los usuarios:
@@ -47,8 +85,23 @@ const getAllUsersHandler = async(req,res) => {
 }
 
 module.exports = {
-    createUserHandler,
+    signup,
     deleteUserHandler,
     updateUserHanlder,
     getAllUsersHandler,
+    login,
+    user,
+    ensureToken
 }
+
+
+/* const updateUserHanlder = async (req,res) => {
+    const { id } = req.params;
+    const { name, lastName, email, birthDate, password, phone, image, address, gender } = req.body;
+    try {
+        await updateUser(id, {name, lastName, email, birthDate, password, phone, image, address, gender });
+        res.status(200).json(`Usuario ${name} actualizado con éxito!`);
+    } catch (error) {
+        res.status(400).json(`Error al actualizar usuario: `, {error:error.message});
+    }
+} */
