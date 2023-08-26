8f94694e03f1cd3a503af25936b30b35b0b697be
PF-Henry2023 <141773841+PF-Henry2023@users.noreply.github.com>
2023-08-26 11:45:42 -0500

    Notificaciones Via Email (#34)
    Co-authored-by: Henry Corredor González <henry.corredor7570@gmail.com>

--- server/src/Utils/genericFunctions.js
+++ server/src/Utils/genericFunctions.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 //validacion para requerir todos los campos al momento de crear el usuario:
 const validateCreateUser = (req, res, next) => {
   const {
@@ -24,6 +26,34 @@ const validateCreateUser = (req, res, next) => {
   next(); // pasa al siguiente middleware
 };
 
+//FUNCIÓN PARA ENVIAR NOTIFICACIONES VIA EMAIL:
+const sendEmailNotification = async (email) => {
+
+  //CODIGO PARA ENVIAR NOTIFICACION VIA EMAIL AL USUARIO QUE HA SIDO CREADO:
+  const transporter = nodemailer.createTransport({
+      host:"smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth:{
+          user:"pfhenry4@gmail.com", //email del servidor o dominio desde donde se envia el mensaje
+          pass:"tndvsvitmutasmpv", // contraseña que se genero en gestionar contraseñas para aplicaciones en google
+      },
+  });
+
+  const link = "http://localhost:5173/"
+
+  await transporter.sendMail({
+      from: '"NUTRITION WEB" <pfhenry4@gmail.com>', // aqui va el correo desde donde se envia el mensaje y titulo de origen
+      to: email, // el email que recibe el mensaje
+      subject:"PRUEBA DE MI PAGINA 1", // titulo del mensaje
+      // text: "hola esto es una prueba", se puede enviar el mensaje como texto plano o como html
+      html: `
+          <b>Por favor de click en el siguiente link para verificar su cuenta</b>
+          <a href="${link}">${link}</a>
+      `// se le puede poner cualquier link que se desee en el cuerpo del mensaje para que cuando ingrese lo redirija puede ser a la plataforma
+  });
+};
 module.exports = {
   validateCreateUser,
+  sendEmailNotification,
 };
