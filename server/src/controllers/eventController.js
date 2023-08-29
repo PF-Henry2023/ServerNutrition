cd773752b2b9f81a76034922b1b9123660d6d12e
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2023-08-29 18:59:32 -0500

    Modelo Citas (Event), con sus rutad definidas, (create, delete, update, get, getById) (#37)
    * ruta para crear citas
    
    * Finalizando Rutas para crear, modificar, eliminar, y obtener citas

---
+++ server/src/controllers/eventController.js
@@ -0,0 +1,48 @@
+const { Event } = require("../db");
+
+//Creación de una cita:
+const createEvent = async (date, hour, purpose) => {
+    const newEvent = await Event.create({ date, hour, purpose });
+    return newEvent;
+}
+
+//Eliminar una cita:
+const deleteEvent = async (id) => {
+    await Event.destroy({ where: {id: id}});
+    return `Cita con id: ${id} eliminada con éxito`
+}
+
+//Actualizar una cita:
+const updateEvent = async (id, date, hour, purpose) => {
+    const event = await Event.findByPk(id);
+    if(!event) throw Error("Cita no encontrada")
+    await event.update({date, hour, purpose});
+    return `Cita modificada con éxito`
+}
+
+//Obtener cita por id:
+const getEventById = async (id) => {
+    const eventFound = await Event.findOne({ where: { id }});
+    if(!eventFound) return { error: "No existe la cita asociada a ese ID"};
+    return {
+        id: eventFound.dataValues.id,
+        fecha: eventFound.dataValues.date,
+        hora: eventFound.dataValues.hour,
+        motivo: eventFound.dataValues.purpose,
+        IdNutricionista: eventFound.dataValues.NutritionistId,
+    }
+}
+
+//obtener todas las citas:
+const getAllEvents = async () => {
+    const allEvents = await Event.findAll();
+    if(allEvents.length === 0) throw Error("¡No hay citas creadas en la base de datos");
+    return allEvents;
+}
+module.exports = {
+    createEvent,
+    deleteEvent,
+    updateEvent,
+    getEventById,
+    getAllEvents,
+}
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,17 +1,20 @@
 //index todas las rutas
 const { Router } = require("express");
 const router = Router();
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const usersRouter = require("./usersRouter");
+const eventsRouter = require("./eventsRouter");
 const calendarRoute = require("./integrationCalendar");
 const paymentRouter = require("./paymentRouter");
 const nutritionistRouter = require("./nutritionistRouter");
 const wellnessPlanRouter = require("./wellnessPlanRouter");
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
 router.use("/users", usersRouter);
+router.use("/events", eventsRouter);
 router.use("/wellnessPlans", wellnessPlanRouter);
 router.use("/nutritionists", nutritionistRouter);
 router.use("/calendarGoogle", calendarRoute);
