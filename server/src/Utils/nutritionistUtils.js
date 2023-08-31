fc43b5b97136ae2bf757498542b5dfd1a095111a
FrancoQ <quenallatafranco@gmail.com>
2023-08-31 11:34:20 -0300

    horarios combinados (#43)
    Co-authored-by: Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>

---
+++ server/src/Utils/nutritionistUtils.js
@@ -0,0 +1,42 @@
+const { Nutritionist } = require("../db");
+
+const getHorarioTrabajoCombinado = async () => {
+  try {
+    const nutricionistasDesdeBD = await Nutritionist.findAll({
+      where: { isActive: true },
+    });
+
+    const horarioCombinado = {
+      diasDeTrabajoCombinados: [],
+      horarioDeTrabajo: {},
+    };
+
+    const conjuntoDiasUnicos = new Set();
+
+    for (let i = 0; i < nutricionistasDesdeBD.length; i++) {
+      const nutricionistaJSON = nutricionistasDesdeBD[i].toJSON();
+      const diasDeTrabajo = nutricionistaJSON.diasDeTrabajo;
+
+      for (const dia of diasDeTrabajo) {
+        conjuntoDiasUnicos.add(dia);
+        const horarioTrabajoArr = nutricionistaJSON.horarioDeTrabajo[dia];
+
+        if (!horarioCombinado.horarioDeTrabajo[dia]) {
+          horarioCombinado.horarioDeTrabajo[dia] = [];
+        }
+
+        horarioCombinado.horarioDeTrabajo[dia] =
+          horarioCombinado.horarioDeTrabajo[dia].concat(horarioTrabajoArr);
+      }
+    }
+
+    horarioCombinado.diasDeTrabajoCombinados = Array.from(conjuntoDiasUnicos);
+
+    return horarioCombinado;
+  } catch (error) {
+    return new Error(error);
+  }
+};
+
+const calcularPuntosNutricionista = () => {};
+module.exports = { getHorarioTrabajoCombinado, calcularPuntosNutricionista };
