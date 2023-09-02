d7c2f5504c31e5a967ea23898e96e006aaafba6b
Quena <quenallatafranco@gmail.com>
2023-09-02 15:01:53 -0300

    horarios Nutricionistas combinado

--- server/src/Utils/nutritionistUtils.js
+++ server/src/Utils/nutritionistUtils.js
@@ -10,12 +10,12 @@ const getHorarioTrabajoCombinado = async () => {
 
     for (let i = 0; i < nutricionistasDesdeBD.length; i++) {
       const diasDeTrabajo = nutricionistasDesdeBD[i].toJSON().diasDeTrabajo; //[[]]
-    }
-    for (const day in diasDeTrabajo) {
-      if (!horarioCombinado[day]) {
-        horarioCombinado[day] = [];
+      for (const day in diasDeTrabajo) {
+        if (!horarioCombinado[day]) {
+          horarioCombinado[day] = [];
+        }
+        horarioCombinado[day].push(...diasDeTrabajo[day]);
       }
-      horarioCombinado[day].push(diasDeTrabajo[day]);
     }
     return horarioCombinado;
   } catch (error) {
