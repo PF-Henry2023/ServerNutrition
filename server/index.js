4b6d26b1cef04bb2dc91c0e945dc3c457beca476
Henry Corredor González <henry.corredor7570@gmail.com>
2023-09-06 10:55:58 -0500

    Cloudinary Exitoso

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app");
 const { sequelize } = require("./src/db");
 const PORT = 3001;
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server raise in PORT: ${PORT}`);
   });
